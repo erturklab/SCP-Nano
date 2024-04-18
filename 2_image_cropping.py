a100611cf1abadcb459572387784f13d7e89b496
Ying Chen <79832992+YingChen7@users.noreply.github.com>
2024-04-18 20:27:09 +0200

    Update 2_image_cropping.py

--- 2_image_cropping.py
+++ 2_image_cropping.py
@@ -5,21 +5,13 @@ from utils import cut_volume_single_tiff
 dir_wholebody_data = ""
 # name of organ to crop into patches 
 organ_name = "" # lung, liver, heart, head, spleen, kidney
+
 # set the patch size and overlap size between patches
-# set the cropping start and end coordinates, specifying a bounding box region to do the cropping
-
-# !!!Setting requirement: cropping_end - cropping_start - patch_overlap = n * (patch_size-patch_overlap), n is an iteger
-# The order is [x_dim, y_dim, z_dim]
-patch_size = [418, 460, 600] 
-patch_overlap = [19, 15, 0]
-cropping_start = [0, 0, 0]
-cropping_end = [7600, 1350, 600]
-
+patch_size = [300, 300, 300] 
+patch_overlap = [30, 30, 30]
 
 parameters = {}
 parameters["file_format"] = "Nifti"
-parameters["region_name"] = "signal" 
-parameters["thumbnail_downsampling"] = 0.1 
 parameters["dataset"] = {}
 parameters["dataset"]["channelname"] = ""
 parameters['dataset']['sourcefolder'] = os.path.join(dir_wholebody_data, "organ_results", f"organ_{organ_name}_raw", "")
@@ -30,10 +22,7 @@ parameters['dataset']['downsampling'] = 'None'
 parameters['partitioning'] = {}
 parameters['partitioning']['patch_size'] = patch_size
 parameters['partitioning']['patch_overlap'] = patch_overlap
-parameters['partitioning']['cropping_offset'] = cropping_start
-parameters['partitioning']['cropping_boundingbox'] = cropping_end
 parameters['partitioning']['safe_cache'] = False
-parameters['multiprocessing'] = False
 parameters['advanced'] = {}
 parameters['advanced']['empty_patches'] = [] 
 cut_volume_single_tiff.cut_volume(parameters)
