788f552414320076855c59ae6a60218a2641dcb1
Ying Chen <79832992+YingChen7@users.noreply.github.com>
2024-04-20 11:30:18 +0200

    Update 5_seg_rebuild.py

--- 5_seg_rebuild.py
+++ 5_seg_rebuild.py
@@ -4,12 +4,13 @@ import numpy as np
 from libtiff import TIFFimage
 from utils import filehandling
 
-def rebuild_organ_seg_from_patch(path_patches_pred, path_patch_region, path_output, path_output_nifti, organ_name, threshold=0.5):
+def rebuild_organ_seg_from_patch(path_patches_pred, path_patch_region, path_organmask_slice, path_output, path_output_nifti, organ_name, organ_key, threshold=0.5):
     """
     Get the prediction of whole organ by assembling prediction of organ patches
     
     path_patches_pred: path of segmentation predictions of organ patches
     path_patch_region: pickle file storing patch coordinates that was generated before in the patch cropping step
+    path_organmask_slice: path of organ mask image sequence 
     path_output: path to save outpout, including organ segmentation image sequence, and maximum intensity projection of whole organ segmentation
     path_output_nifti: path to organ segmentation result as nifti file
     organ_name: the name of the organ
@@ -65,6 +66,12 @@ def rebuild_organ_seg_from_patch(path_patches_pred, path_patch_region, path_outp
     print(f"{datetime.datetime.now()} Setting everything to 1")
     volume[volume>threshold]=1
     volume = volume.astype(np.uint8)
+    for i in range(volume.shape[-1]):
+        img_organmask = cv2.imread(os.path.join(path_organmask_slice, sorted(os.listdir(path_organmask_slice))[i]), -1)
+        img_organmask = np.squeeze(img_organmask)
+        img_organmask[img_organmask!=organ_key]=0
+        img_organmask[img_organmask==organ_key]=1
+        volume[:, :, i] = volume[:, :, i] * img_organmask
 
     print(f"{datetime.datetime.now()} Calculating MIP")
     mip = np.max(volume, axis=2)
@@ -83,12 +90,27 @@ def rebuild_organ_seg_from_patch(path_patches_pred, path_patch_region, path_outp
 # Root of whole-body image data    
 dir_wholebody_data = ""
 # name of organ to crop into patches 
-organ_name = ""
+cur_organ_name = ""
 
-path_region          = os.path.join(dir_wholebody_data, "organ_results", f"organ_{organ_name}_crop", "sync_C01region.pickledump")
-path_patches_pred    = os.path.join(dir_wholebody_data, "organ_results", f"organ_{organ_name}_crop", "local_C01_norm_infer")
-path_out             = os.path.join(dir_wholebody_data, "organ_results", f"organ_{organ_name}_crop")
-path_nifti_out       = os.path.join(dir_wholebody_data, "organ_results", f"organ_{organ_name}_crop", f"organ_{organ_name}_pred.nii.gz")
+path_organ_keys           = os.path.join(dir_wholebody_data, "organ_keys.txt")
+keys_dict = {}
+f_organ_keys = open(path_organ_keys)
+line = f_organ_keys.readline()
+while line:
+    organ_line = line.replace('\n', '')
+    organ_key = organ_line.split(":")[0]
+    organ_name = organ_line.split(":")[1]
+    keys_dict[organ_key] = organ_name
+    line = f_organ_keys.readline()
+organ_keys_list = list(keys_dict.keys())
+organ_names_list = list(keys_dict.values())
+cur_organ_key = int(organ_keys_list[organ_names_list.index(cur_organ_name)])
 
-rebuild_organ_seg_from_patch(path_patches_pred=path_patches_pred, path_patch_region=path_region,
-                             path_output=path_out, path_output_nifti=path_nifti_out, organ_name=organ_name)
+path_region          = os.path.join(dir_wholebody_data, "organ_results", f"organ_{cur_organ_name}_crop", "sync_C01region.pickledump")
+path_organmask_slice = os.path.join(dir_wholebody_data, "organ_results", f"organ_{cur_organ_name}_mask")
+path_patches_pred    = os.path.join(dir_wholebody_data, "organ_results", f"organ_{cur_organ_name}_crop", "local_C01_norm_infer")
+path_out             = os.path.join(dir_wholebody_data, "organ_results", f"organ_{cur_organ_name}_crop")
+path_nifti_out       = os.path.join(dir_wholebody_data, "organ_results", f"organ_{cur_organ_name}_crop", f"organ_{cur_organ_name}_pred.nii.gz")
+
+rebuild_organ_seg_from_patch(path_patches_pred=path_patches_pred, path_patch_region=path_region, path_organmask_slice = path_organmask_slice,
+                             path_output=path_out, path_output_nifti=path_nifti_out, organ_name=cur_organ_name, organ_key=cur_organ_key)
